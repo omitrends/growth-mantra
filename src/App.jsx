ff98e97c19b68f61e84a5b977abc3b8041a17bc6
Radhika-2010 <radhika2003deshpande@gmail.com>
2025-03-05 12:20:33 +0530

    login page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar"
 import Slider from "./components/Slider"
+import Login from "./components/Login"
+import "./components/Login.css";
 
 function App(){
   return (
   <div>
     <Navbar />
     <Slider/>
+    <Login />
   </div>
   );
 }
