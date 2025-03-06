6b1be4b17ee8788e8d7dee1cd3af9e6b09180e7a
Radhika-2010 <radhika2003deshpande@gmail.com>
2025-03-06 13:49:57 +0530

    branches merged

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/dashboard";
 // import ForgetPass from "./components/forgetPass";
+import Fitness from "./components/Fitness";
 function App() {
   return (
     <div>
@@ -17,6 +18,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/get-started" element={<CreateAccount/>}></Route>
             <Route path="/dashboard" element={<Dashboard/>}></Route>
+            <Route path="/Fitness" element={<Fitness/>}></Route>
 
           </Route>
         </Routes>
